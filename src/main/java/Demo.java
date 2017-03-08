492faab56baf5624f4f222e2e895599d5f7b7aa4
wangjie <1152689483@qq.com>
2017-03-08 20:59:04 +0800

    Shiro项目完善

---
+++ src/main/java/Demo.java
@@ -0,0 +1,13 @@
+/**
+ * Title:
+ * Description:
+ * <p>
+ * Project: MyShiroApplication
+ * Create User: 王杰
+ * Create Time: 2017/3/8
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("HELLO World");
+    }
+}
